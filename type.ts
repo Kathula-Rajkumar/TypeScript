9e4455ddb870e22b9e8de87e202d2497ba1d4a26
Rajkumar Kathula <rajkumar.kathula15@gmail.com>
2025-07-17 23:23:35 +0530

    when to use types and interfaces in ts

--- type.ts
+++ type.ts
@@ -6,3 +6,11 @@ function greet(id:GreetArg){
 
 greet(1);
 greet("1")
+
+
+- Rule of thumb
+
+1.Use interface when you’re designing object-oriented code or working with classes.(names, age, objects and etc..)
+
+2.Use type when you need to describe more than just an object — like functions, primitives, or union types.
+
