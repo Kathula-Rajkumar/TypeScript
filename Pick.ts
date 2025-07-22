0b58704cc1fa565c1db0c85880dd90ac9cb4c80f
Rajkumar Kathula <rajkumar.kathula15@gmail.com>
2025-07-22 23:52:16 +0530

    Pick.ts
    Pick is a utility type used to create a new type by selecting specific properties from an existing interface or type.

---
+++ Pick.ts
@@ -0,0 +1,58 @@
+->Pick is a utility type used to create a new type by selecting specific properties from an existing interface or type.
+
+
+interface User {
+  name: string;
+  age: number;
+  email: string;
+  password: number;
+}
+
+// Define the fields you want to allow updating
+type updatedUser = Pick<User, "name" | "email" | "password">;
+
+
+function updatedUserProfile(original: User, updated: updatedUser) {
+  console.log("Previous User Info:");
+  console.log("Name:", original.name);
+  console.log("Email:", original.email);
+  console.log("Password:", original.password);
+  console.log("Age:", original.age);
+
+  console.log("\n Updated User Info:");
+  console.log("Name:", updated.name);
+  console.log("Email:", updated.email);
+  console.log("Password:", updated.password);
+}
+
+
+const previousUser: User = {
+  name: "Raj Kumar",
+  age: 22,
+  email: "oldemail@gmail.com",
+  password: 111
+};
+
+
+const updatedData: updatedUser = {
+  name: "Rajkumar",
+  email: "rajkumar@gmail.com",
+  password: 123
+};
+
+// Calling the function
+updatedUserProfile(previousUser, updatedData);
+
+
+
+//output 
+Previous User Info:
+Name: Raj Kumar
+Email: oldemail@gmail.com
+Password: 111
+Age: 22
+
+Updated User Info:
+Name: Rajkumar
+Email: rajkumar@gmail.com
+Password: 123
